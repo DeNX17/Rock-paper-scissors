18bb6384bfae3d501d7f092cf99c57d0dd54b115
Artem <hsmail1742@mail.ru>
2018-09-25 18:09:12 +0400

    fix

--- src/main.js
+++ src/main.js
@@ -2,13 +2,7 @@ import Vue from 'vue';
 import App from './App.vue';
 
 import {store} from './store';
-import {router} from './routes.js';
-//import axios from 'axios';
-/*
-import VueResource from 'vue-resource';
 
-Vue.use(VueResource);*/
-//Vue.http.options.root = 'http://js.dmitrylavrik.ru/api/
 new Vue({
   el: '#app',
   store,
